5300263d866228562a4fd76df32427950604bdb3
Shalini-Sahay <shalinisahay1508@gmail.com>
2021-10-27 14:41:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Users from "./Component/card.js";
+import "./styles.css";
+import img1 from './img1/logo.png';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users_data: [], loading: true };
+    this.showUsers = this.showUsers.bind(this);
+  }
+
+  showUsers() {
+    document.getElementById("main").style.display = "flex";
+    const source = "https://reqres.in/api/users?page=1";
+    fetch(source)
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ users_data: users.data, loading: false });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark  m-3">
+          <div className="container">
+            <span>
+              <img src={img1} alt=""/>
+            </span>
+            <button onClick={this.showUsers}>Get Users</button>
+          </div>
+        </nav>
+
+        <Users loading={this.state.loading} users={this.state.users_data} />
+      </>
+    );
+  }
 }
 
 export default App;
